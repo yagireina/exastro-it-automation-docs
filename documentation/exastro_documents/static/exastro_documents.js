52df0bcf2a5cec6178d1ff6f9d461a15f645fcb8
Wreath Vine <wreathvine@gmail.com>
2023-01-09 15:53:39 +0000

    目次追加

--- documentation/exastro_documents/static/exastro_documents.js
+++ documentation/exastro_documents/static/exastro_documents.js
@@ -6,6 +6,10 @@ $('#menuButton').on('click', function(){
   $('#sideMenu').toggleClass('on');
 });
 
+$('#tocButton').on('click', function(){
+ $('#tableOfContents').toggleClass('on');
+});
+
 /*
 Page Top Link
 */
@@ -50,4 +54,54 @@ $( window ).on('load', function(){
     }
   });
 
+  
+  const headings = document.querySelectorAll('h1,h2,h3,h4,h5,h6');
+  let toc = '<ol id="fixed-toc">';
+  headings.forEach( function(heading, index) {
+    heading.id = 'heading-0' + (index+1);
+    let id = heading.id;
+    toc = toc + '<li><a href="#' + id + '">' + heading.textContent + '</a></li>';
+  });
+  toc = toc + '</ol>';
+  let set_position = 0;
+  window.addEventListener('scroll', function() {
+      if (set_position < document.documentElement.scrollTop) {
+          direction= 'down';
+      } else {
+          direction= 'up';
+      }
+      set_position = document.documentElement.scrollTop;
+      let scroll = window.scrollY;//スクロール量を取得
+      let height = window.innerHeight;//画面の高さを取得
+      let offset = height - 100;
+      // const toc_completed = document.getElementById('fixed-toc');
+      const targets = document.querySelectorAll('#tableOfContents li');
+      headings.forEach( function( heading , index) {
+          let i = index + 1;
+          let target = targets[i];
+          let pos = heading.getBoundingClientRect().top + scroll;//見出しの位置
+          if ( scroll >pos - height + offset ) {//スクロール量が見出しを超えた
+              if (headings[index + 1] !== undefined){// 次の見出しがある＝最後の見出しではない
+                  let next_pos = headings[index + 1].getBoundingClientRect().top + scroll;//次の見出しの位置
+                  if ( scroll > next_pos - height + offset ) { // スクロール量が次の見出しも超えている
+                      target.classList.remove('current');
+                  } else if (target.classList.contains('current') == true) { // すでにcurrentがついている
+                      return;
+                  } else if ( i == 1 ){// 1つ目
+                      target.classList.add('current')
+                  } else { // 次の見出しは見えてない
+                      target.classList.add('current');
+                  }
+              } else { //最後の見出しの時
+                  if(target != undefined ) {
+                      target.classList.add('current');
+                  }
+              }
+          } else { //スクロール量が見出しを超えてない
+              if(target != undefined ) {
+                  target.classList.remove('current');
+              }
+          }
+      });
+  });
 });
